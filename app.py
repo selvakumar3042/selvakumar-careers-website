4e1d17c71aef1cb43fa922ada99664f5cb950486
selvakumar3042 <63241685+selvakumar3042@users.noreply.github.com>
2023-04-19 16:56:09 +0000

    Html pages
    Html page creation

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-print (__name__)
+
+
 @app.route("/")
 def hello():
-  return "Hello world"
+  return render_template('homepage.html')
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
-  
- 
+  app.run(host='0.0.0.0', debug=True)
