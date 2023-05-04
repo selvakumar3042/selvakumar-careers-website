37b401b3c0ae1c541cb9cbc3cd5f4c904480d081
selvakumar3042 <63241685+selvakumar3042@users.noreply.github.com>
2023-05-04 01:22:40 +0000

    commit

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def list_jobs():
   return jsonify(JOBS)
 
 
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', debug=True)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
